97bc8b277ba0246dc4cc27d4b608fc9571e0706d
sdatla04 <43227239+sdatla04@users.noreply.github.com>
2018-09-17 13:20:59 -0400

    made changes remote

--- Vidly082018/Models/Customer.cs
+++ Vidly082018/Models/Customer.cs
@@ -38,7 +38,7 @@ namespace Vidly082018.Models
         //Added the comment to the new branch feature that was created - 9/17/2018
 
         // Made changes remotely
-        // Making changes to raise a conflict
+        // Made changes locally, to raise a conflict
 
     }
 
